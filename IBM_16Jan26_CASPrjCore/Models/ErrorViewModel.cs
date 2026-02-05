b14a873d3835811a99722682479327ee9aac8be0
EZLLeno <EZLLeno@EZL-LEN>
2026-02-05 15:41:26 +0530

    Add project files.

---
+++ IBM_16Jan26_CASPrjCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IBM_16Jan26_CASPrjCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
