2ef2202fface002b3d695c2855e3e367ea83c496
EZLLeno <EZLLeno@EZL-LEN>
2026-02-05 15:45:50 +0530

    Added Parient Controller

---
+++ IBM_16Jan26_CASPrjCore/Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IBM_16Jan26_CASPrjCore.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
