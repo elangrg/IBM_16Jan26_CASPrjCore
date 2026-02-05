5c3f84f2f857994b3df1058df11e0b7afab7dcc6
EZLLeno <EZLLeno@EZL-LEN>
2026-02-05 15:55:46 +0530

    Doc ctr Added

---
+++ IBM_16Jan26_CASPrjCore/Controllers/DoctorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IBM_16Jan26_CASPrjCore.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
